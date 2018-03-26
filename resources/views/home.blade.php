7453ac63c2fe5e5479aa5a15758806ff87417468
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-03-26 21:43:12 +0200

    home page design afgewerkt (voor zover nu mogelijk)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,22 +11,24 @@
 </head>
 <body>
 
-    <header>
-        <nav>
-            <ul>
-                <li><a href="#">SEMESTR</a></li>
-                <li><a href="#" class="active">around me</a></li>
-                <li><a href="#">crossings</a></li>
-                <li><a href="#">settings</a></li>
-                <li class="nav_profile"><a href="#">profile</a></li>
-            </ul>
-        </nav>
+    <div id="header_wrapper">
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">SEMESTR</a></li>
+                    <li><a href="#" class="active">around me</a></li>
+                    <li><a href="#">crossings</a></li>
+                    <li><a href="#">settings</a></li>
+                    <li class="nav_profile"><a href="#">profile</a></li>
+                </ul>
+            </nav>
 
-        <div class="location">
-            <h1 class="location_city">New York</h1>
-            <div class="location_label">current location</div>
-        </div>
+            <div class="location">
+                <h1 class="location_city">New York</h1>
+                <div class="location_label">current location</div>
+            </div>
     </header>
+    </div>
 
     <div class="main">
 
@@ -46,25 +48,25 @@
             </div>
 
             <div class="aroundme_item main_item">
-                <img class="aroundme_item_image">
-                <div class="aroundme_item_name">Chester Cross</div>
-                <div class="aroundme_item_detail">
+                <img class="aroundme_item_image" src="{{url('img/profile_pic_default.jpg')}}">
+                <div class="aroundme_item_right aroundme_item_name">Amber Heard</div>
+                <div class="aroundme_item_right aroundme_item_detail">
                     <div class="aroundme_item_school">student at NYU</div>
                     <div class="aroundme_item_age">27 years</div>
                     <div class="aroundme_item_country">United Kingdom</div>
                 </div>
-                <div class="aroundme_item_intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</div>
+                <div class="aroundme_item_right aroundme_item_intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</div>
             </div>
 
             <div class="aroundme_item main_item">
-                <img class="aroundme_item_image">
-                <div class="aroundme_item_name">Chester Cross</div>
-                <div class="aroundme_item_detail">
+                <img class="aroundme_item_image" src="{{url('img/profile_pic_default.jpg')}}">
+                <div class="aroundme_item_right aroundme_item_name">Amber Heard</div>
+                <div class="aroundme_item_right aroundme_item_detail">
                     <div class="aroundme_item_school">student at NYU</div>
                     <div class="aroundme_item_age">27 years</div>
                     <div class="aroundme_item_country">United Kingdom</div>
                 </div>
-                <div class="aroundme_item_intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</div>
+                <div class="aroundme_item_right aroundme_item_intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</div>
             </div>
 
         </section>
@@ -74,20 +76,20 @@
 
             <!-- nu placeholders, later loopen we hier chat items uit en vullen we de juiste zaken in uit de database -->
             <div class="chat_item sidebar_item">
-                <img class="chat_item_image">
-                <div class="chat_item_name">Bernice Hampton</div>
-                <div class="chat_item_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</div>
-            </div>
-
-            <div class="chat_ite sidebar_item">
-                <img class="chat_item_image">
-                <div class="chat_item_name">Bernice Hampton</div>
+                <img class="chat_item_image" src="{{url('img/profile_pic_default.jpg')}}">
+                <div class="chat_item_name">Amber Heard</div>
                 <div class="chat_item_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</div>
             </div>
 
             <div class="chat_item sidebar_item">
-                <img class="chat_item_image">
-                <div class="chat_item_name">Bernice Hampton</div>
+                <img class="chat_item_image" src="{{url('img/profile_pic_default.jpg')}}">
+                <div class="chat_item_name">Amber Heard</div>
+                <div class="chat_item_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</div>
+            </div>
+
+            <div class="chat_item sidebar_item">
+                <img class="chat_item_image" src="{{url('img/profile_pic_default.jpg')}}">
+                <div class="chat_item_name">Amber Heard</div>
                 <div class="chat_item_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</div>
             </div>
         </section>
