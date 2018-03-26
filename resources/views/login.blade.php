07e53d1fc4128606d3d0dbc3fbaaac341c65da54
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-03-26 22:05:16 +0200

    eerste setup login page ok

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,9 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Semestr</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
-<h1>Hello bloody world</h1>
-<a href="/login/facebook">Login with facebook</a>
+    <img class="main_logo" alt="Semestr logo" src="{{url('img/semestr_logo1.png')}}">
+
+    <div class="login">
+
+        <a class="social_login facebook_login" href="/login/facebook">Login with Facebook</a>
+        <a class="social_login twitter_login" href="/login/twitter">Login with Twitter</a>
+        <a class="social_login google_login" href="/login/google">Login with Google+</a>
+
+    </div>
 </body>
 </html>
