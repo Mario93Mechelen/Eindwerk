5013604cb634039cec7404f1539f7a086d9d8fd4
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-04-07 12:07:50 +0200

    header blade apart gezet en kleine correcties

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,18 @@
+<div id="header_wrapper">
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">SEMESTR</a></li>
+                <li><a href="#" class="active">around me</a></li>
+                <li><a href="#">crossings</a></li>
+                <li><a href="#">settings</a></li>
+                <li class="nav_profile"><a href="#">profile</a></li>
+            </ul>
+        </nav>
+
+        <div class="location">
+            <h1 class="location_city">{{$location ? $location->city : "no location found"}}</h1>
+            <div class="location_label">current location</div>
+        </div>
+    </header>
+</div>
