6b31629675d7cbd90819de598b7e36a90452c5fa
Mario93Mechelen <r0334050@student.thomasmore.be>
2018-05-07 14:36:16 +0200

    added javascript function for location

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -73,39 +73,25 @@
     <script>
         var longitude;
         var latitude;
-        $.ajaxSetup({
 
-            headers: {
-
-                'X-CSRF-TOKEN': "{{csrf_token()}}",
-
-            }
-
-        });
-        $.ajax({
-            method:"POST",
-            url:"{{URL::action('LocationController@getLocation')}}",
-            data:{
-                'key': 'AIzaSyBvs7EHp5iJ5aaCe-k2DodKcTyzFtbqrdw',
-            }
-        }).done(function(response){
-            if(response.code==200){
-                latitude = response.res.location.lat;
-                longitude = response.res.location.lng;
-                storeLocation(latitude,longitude);
-                initMap(latitude,longitude);
-            }
-        });
-        /*function getLocation() {
+        function getLocation() {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                alert("Geolocation is not supported by this browser.");
             }
         }
         function showPosition(position) {
-            latitude =  position.coords.latitude;
-            longitude =  position.coords.longitude;
-            console.log(latitude +' '+ longitude);*/
-            function storeLocation(latitude,longitude){
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+            console.log(latitude+":"+longitude)
+            storeLocation(latitude, longitude);
+        }
+
+        getLocation();
+        window.setInterval(getLocation, 300000);
+
+        function storeLocation(latitude,longitude){
             $.ajaxSetup({
 
                 headers: {
@@ -115,37 +101,24 @@
                 }
 
             });
-                $.ajax({
-                    method:"POST",
-                    url:"{{URL::action('LocationController@store')}}",
-                    data:{
-                        'longitude': longitude,
-                        'latitude': latitude,
-                    }
-                }).done(function(response){
-                    if(response.code==200) {
+            $.ajax({
+                method:"POST",
+                url:"{{URL::action('LocationController@store')}}",
+                data:{
+                    'longitude': longitude,
+                    'latitude': latitude,
+                }
+            }).done(function(response){
+                if(response.code==200) {
+                    if(response.res.results[0]) {
                         console.log(response.res.results[0].address_components[2].long_name);
                         $('.location_city').html(response.res.results[0].address_components[2].long_name);
+                    }else{
+                        $('.location_city').html("seems like we couldn't find your location");
                     }
-                });
-        }
-        //getLocation();
-        //http://maps.googleapis.com/maps/api/geocode/json?sensor=false&language=en&latlng=51.0350601,4.4531024
-        function initMap(latitude,longitude) {
-            var pos = {lat:latitude, lng: longitude};
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 15,
-                center: pos
+                }
             });
-            var marker = new google.maps.Marker({
-                position: pos,
-                map: map
-            });
-
         }
-    </script>
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMTFMU3WYZ1vlAuw2BGnuDgdsaIu5cdd0&callback=initMap">
     </script>
 
     @endsection
