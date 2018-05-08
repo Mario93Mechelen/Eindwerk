26f6a9ed28eb66c3cd3c814f2f2a07d5f3f5eb63
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-05-08 22:09:04 +0200

    header avatar fix and init new styling

--- resources/views/partials/header_old.blade.php
+++ resources/views/partials/header_old.blade.php
@@ -6,14 +6,13 @@
                 <li><a href="#" class="active">around me</a></li>
                 <li><a href="#">crossings</a></li>
                 <li><a href="#">settings</a></li>
-                <li class="nav_profile"><a href="#" style="background-image: url({{$myUser->avatar}});"></a></li>
+                <li class="nav_profile"><a href="#">{{$myUser->avatar}}</a></li>
             </ul>
         </nav>
-        <!--
+
         <div class="location">
-            <h1 class="location_city">hier var met locatie</h1>
+            <h1 class="location_city">{{isset($location) ? $location->city : "no location found"}}</h1>
             <div class="location_label"><i class="fas fa-location-arrow"></i> current location</div>
         </div>
-        -->
     </header>
 </div>
