20cfb7a81edfa68e2316b169ce3c36832590e75c
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-05-09 15:13:55 +0200

    nav menu fixed width, customizable scrollbar & some init styling done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,20 @@
 <div id="header_wrapper">
     <header>
-        <nav>
-            <ul>
-                <li><a href="#">SEMESTR</a></li>
-                <li><a href="#" class="active">around me</a></li>
-                <li><a href="#">crossings</a></li>
-                <li><a href="#">settings</a></li>
-                <li class="nav_profile"><a href="#" style="background-image: url({{$myUser->avatar}});"></a></li>
-            </ul>
+
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">home</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">around me</a></li>
+                    <li><a href="#">crossings</a></li>
+                    <li><a href="#">settings</a></li>
+                    <li class="nav_profile"><a class="nav_profile_link" href="#" style="background-image: url('{{$myUser->avatar}}')"></a></li>
+                </ul>
+            </div>
         </nav>
+
         <!--
         <div class="location">
             <h1 class="location_city">hier var met locatie</h1>
