1b40741d47dd380a69b69d0683c0343a92b9308d
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-05-10 23:41:31 +0200

    reworked navigation: mobile bottom section done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,12 +8,14 @@
                 <li class="nav_profile"><a href="#"><div class="profile_pic" style="background-image: url('{{$myUser->avatar}}');"></div>
                         <p>profile</p></a></li>
             </ul>
-            <ul class="nav_left">
-                <li class="nav_semestr"><a href="#">semestr</a></li>
-                <li class="nav_aroundme"><a href="#">around me</a></li>
-                <li class="nav_crossings"><a href="#">crossings</a></li>
-                <li class="nav_groups"><a href="#">groups</a></li>
-            </ul>
+            <div class="nav_left_wrapper">
+                <ul class="nav_left">
+                    <li class="nav_semestr"><a href="#">semestr</a></li>
+                    <li class="nav_aroundme"><a href="#">around me</a></li>
+                    <li class="nav_crossings"><a href="#">crossings</a></li>
+                    <li class="nav_groups"><a href="#">groups</a></li>
+                </ul>
+            </div>
         </nav>
 
     </header>
