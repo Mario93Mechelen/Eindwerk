3752a6412f3948fa43906b4c10ed7f34de7a32fe
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-05-10 16:30:02 +0200

    login page styling mostly done, details to be finished

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,28 +9,30 @@
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/main.css">
 </head>
-<body class="login_page">
+<body class="login_page_wrapper" style="background-image: url('img/landing-banner-mobile.jpg')">
 
-    <img class="main_logo" alt="Semestr logo" src="{{url('img/Semestr_logo1.png')}}">
+    <div class="login_page">
+        <img class="main_logo" alt="Semestr logo" src="{{url('img/Semestr_logo1.png')}}">
 
-            <div class="login-page-center">
+        <div class="login-page-center">
 
-                <form class="login-form">
-                    <div class="form-group">
-                        <label for="email">email</label>
-                        <input type="email" class="form-control" id="email" placeholder="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">password</label>
-                        <input type="password" class="form-control" id="password" placeholder="password">
-                    </div>
-                    <button type="submit" class="btn btn-primary">login</button>
-                </form>
+            <form class="login-form">
+                <div class="form-group">
+                    <label for="email">email</label>
+                    <input type="email" class="form-control" id="email" placeholder="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">password</label>
+                    <input type="password" class="form-control" id="password" placeholder="password">
+                </div>
+                <button type="submit" class="btn btn-primary">login</button>
+            </form>
 
-                <div class="fb-login-intro"><span>or login with</span></div>
-                <a class="btn btn-primary social_login facebook_login" href="/login/facebook">facebook</a>
+            <div class="fb-login-intro"><span>or login with</span></div>
+            <a class="btn btn-primary social_login facebook_login" href="/login/facebook">facebook</a>
 
-            </div>
+        </div>
+    </div>
 
 </body>
 </html>
