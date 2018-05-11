464fffcd6e9a80d504ebccf0b4a38cc878ecbe3b
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-05-11 11:54:55 +0200

    reworked navigation: desktop version overall setup done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,9 @@
 
         <nav>
             <ul class="nav_right">
-                <li class="nav_chat"><a href="#"><img src='{{ asset('img/Chat.png') }}'><p>chat</p></a></li>
-                <li class="nav_profile"><a href="#"><div class="profile_pic" style="background-image: url('{{$myUser->avatar}}');"></div>
-                        <p>profile</p></a></li>
+                <li class="nav_chat"><a href="#"><img src='{{ asset('img/Chat.png') }}'><p>chats</p></a></li>
+                <li class="nav_profile"><a href="#"><p>{{$myUser->first_name}}</p><div class="profile_pic" style="background-image: url('{{$myUser->avatar}}');"></div>
+                        </a></li>
             </ul>
             <div class="nav_left_wrapper">
                 <ul class="nav_left">
