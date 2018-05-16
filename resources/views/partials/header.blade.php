5b8b8cad1246a6369e4543901804a384ec7e9009
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-05-16 17:04:16 +0200

    header crossings link functional

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -67,7 +67,7 @@
                     <li class="nav_semestr"><a href="{{URL::action('HomeController@index')}}"><img src='{{ asset('img/Semestr_logo2_gray.png')
                     }}'><p>Semestr</p></a></li>
                     <li class="nav_aroundme"><a href="{{URL::action('HomeController@index')}}">around me</a></li>
-                    <li class="nav_crossings"><a href="#">crossings</a></li>
+                    <li class="nav_crossings"><a href="{{URL::action('ProfileController@index')}}">crossings</a></li>
                     <li class="nav_groups"><a href="#">groups</a></li>
                 </ul>
             </div>
