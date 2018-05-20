4c7994080034f5c29858b16c5c360cfc0368afe6
Mario93Mechelen <r0334050@student.thomasmore.be>
2018-05-20 17:26:32 +0200

    forgot small thing

--- resources/views/crossings/userprofile.blade.php
+++ resources/views/crossings/userprofile.blade.php
@@ -278,6 +278,7 @@
                             $('*[data-id="' + data.data.conversation_id + '"]').find('.active-chat-item-indicator').removeClass('hidden');
                             getChats(response.conversation_id[0]);
                         }else{
+                            $('.active-chat-item-indicator').addClass('hidden');
                             $('#chat_overview').prepend('<div class="item item-list col-xs-12"><a class="item-content chat_to_detail chat-active" href="" data-user="'+response.receiver.id+'" data-id="'+response.conversation_id+'"><div class="active-chat-item-indicator"></div><img class="chat-avatar" src="'+src+'"><div class="chat-right"><div class="chat-nametime"><p class="chat-name">'+response.receiver.first_name+' '+response.receiver.last_name+'</p><p class="chat-time">just now</p></div></div></a></div>')
                             $('.conversation-message-in').remove();$('.conversation-message-out').remove();
                         }
