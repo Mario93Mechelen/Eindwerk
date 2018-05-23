5258b6cdb9982de3e20bfd2974946e4fc7bca939
Mario93Mechelen <r0334050@student.thomasmore.be>
2018-05-23 11:35:53 +0200

    en nu stop ik er mee lol

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -67,7 +67,8 @@
                     $('.chat_to_detail').on('click', function (e) {
                         e.preventDefault();
                         getChats(data.data.conversation_id);
-                    })
+                    });
+                    $(".messages_container").animate({scrollTop: $('.chats-view').prop("scrollHeight")}, 500);
                     console.log(data);
                 } else {
                     console.log('chat is closed');
@@ -86,7 +87,8 @@
                         $('.chat_to_detail').on('click', function (e) {
                             e.preventDefault();
                             getChats(data.data.conversation_id);
-                        })
+                        });
+                        $(".messages_container").animate({scrollTop: $('.chats-view').prop("scrollHeight")}, 500);
                     }
                 }
             }else{
@@ -106,8 +108,10 @@
                     $('.chat_to_detail').on('click', function (e) {
                         e.preventDefault();
                         getChats(data.data.conversation_id);
-                    })
+                    });
                     console.log(data);
+
+                    $(".messages_container").animate({scrollTop: $('.chats-view').prop("scrollHeight")}, 500);
                 } else {
                     console.log('chat is closed');
                     if ($('*[data-id="' + data.data.conversation_id + '"]').length) {
@@ -122,14 +126,17 @@
                         $('.chat_to_detail').on('click', function (e) {
                             e.preventDefault();
                             getChats(data.data.conversation_id);
-                        })
+                        });
+
+                        $(".messages_container").animate({scrollTop: $('.chats-view').prop("scrollHeight")}, 500);
                     } else {
                         $('.active-chat-item-indicator').addClass('hidden');
                         $('#chat_overview').prepend('<div class="item item-list col-xs-12"><a class="item-content chat_to_detail chat-active" href="" data-user="' + data.data.sender.id + '" data-id="' + data.data.conversation_id + '"><div class="active-chat-item-indicator"></div><img class="chat-avatar" src="' + data.data.sender.avatar + '"><div class="online-indicator online"></div><div class="chat-right"><div class="chat-nametime"><p class="chat-name">' + data.data.sender.first_name + ' ' + data.data.sender.last_name + '</p><p class="chat-time">just now</p></div><p class="chat-last-message-start">' + data.data.chat + '</p></div></a></div>');
                         $('.chat_to_detail').on('click', function (e) {
                             e.preventDefault();
                             getChats(data.data.conversation_id);
-                        })
+                        });
+                        $(".messages_container").animate({scrollTop: $('.chats-view').prop("scrollHeight")}, 500);
                     }
                 }
             }
