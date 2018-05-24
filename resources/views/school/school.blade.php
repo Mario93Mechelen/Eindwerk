deac6382fbd6cceae9af01aa640d3d4cc62590fb
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-05-24 23:01:48 +0200

    school page: school name & top toggle switch done

--- resources/views/school/school.blade.php
+++ resources/views/school/school.blade.php
@@ -6,6 +6,25 @@
 
         <div class="cover_image"></div>
 
+        <div class="school_page_content">
+
+            <div class="upper_section">
+                <h2 class="school_name">Thomas More Mechelen Campus Kruidtuin</h2>
+            </div>
+
+            <div class="school-list-toggle">
+                <ul>
+                    <li class="see-school-info active">school info</li>
+                    <li class="see-student-feed">student feed</li>
+                </ul>
+            </div>
+
+
+
+
+
+        </div>  <!-- einde school page content -->
+
     </div>  <!-- einde school page -->
 
 @endsection
