6f10846944b1a39a8e6f137959c6501540bd9c8c
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-05-25 12:08:21 +0200

    school section: student feed mobile done, incl. create new post button

--- resources/views/school/school.blade.php
+++ resources/views/school/school.blade.php
@@ -175,7 +175,106 @@
 
             <div class="student-feed-section">
 
-                fggg
+                <div class="button-wrapper new-post-button-wrapper">
+                    <a href="#" class="button">
+                        <img class="new-post-img" src="/img/profile_pic_default.jpg" alt=""/>
+                        <p>create new post</p>
+                    </a>
+                </div>
+
+                <div class="post student-feed-post">
+                    <div class="post-top post-section">
+                        <img class="post-img" src="/img/profile_pic_default.jpg" alt=""/>
+                        <div class="post-top-right">
+                            <h4 class="post-name">Amber Heard</h4>
+                            <p class="post-time">2 hours ago</p>
+                        </div>
+                    </div>
+                    <div class="post-mid post-section">
+                        <p class="post-message">This is a post message, which was typed as an example of what a post
+                            message might look like, you know. Yeah boiii.</p>
+                    </div>
+                    <div class="post-bottom post-section">
+                        <a href="" class="show-hide-post-comments">show comments</a>
+                    </div>
+                    <div class="post-comment-section post-section">
+
+                        <div class="post-comments">
+                            <div class="post-comment">
+                                <img class="post-comment-img" src="/img/profile_pic_default.jpg" alt=""/>
+                                <div class="post-comment-right">
+                                    <h4 class="post-comment-name">Amber's Sister</h4>
+                                    <p class="post-comment-message">I concur with that statement, this comment is just
+                                        here to acknowledge what was written before.</p>
+                                </div>
+                            </div>
+                            <div class="post-comment">
+                                <img class="post-comment-img" src="/img/profile_pic_default.jpg" alt=""/>
+                                <div class="post-comment-right">
+                                    <h4 class="post-comment-name">Amber's Sister</h4>
+                                    <p class="post-comment-message">I concur with that statement, this comment is just
+                                        here to acknowledge what was written before.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="post-new-comment">
+                            <img class="post-new-comment-img" src="/img/profile_pic_default.jpg" alt=""/>
+                            <form>
+                                <input type="text">
+                                <button type="submit"></button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>  <!-- einde post -->
+
+                <div class="post student-feed-post">
+                    <div class="post-top post-section">
+                        <img class="post-img" src="/img/profile_pic_default.jpg" alt=""/>
+                        <div class="post-top-right">
+                            <h4 class="post-name">Amber Heard</h4>
+                            <p class="post-time">2 hours ago</p>
+                        </div>
+                    </div>
+                    <div class="post-mid post-section">
+                        <p class="post-message">This is a post message, which was typed as an example of what a post
+                            message might look like, you know. Yeah boiii.</p>
+                    </div>
+                    <div class="post-bottom post-section">
+                        <a href="" class="show-hide-post-comments">show comments</a>
+                    </div>
+                    <div class="post-comment-section post-section">
+
+                        <div class="post-comments">
+                            <div class="post-comment">
+                                <img class="post-comment-img" src="/img/profile_pic_default.jpg" alt=""/>
+                                <div class="post-comment-right">
+                                    <h4 class="post-comment-name">Amber's Sister</h4>
+                                    <p class="post-comment-message">I concur with that statement, this comment is just
+                                        here to acknowledge what was written before.</p>
+                                </div>
+                            </div>
+                            <div class="post-comment">
+                                <img class="post-comment-img" src="/img/profile_pic_default.jpg" alt=""/>
+                                <div class="post-comment-right">
+                                    <h4 class="post-comment-name">Amber's Sister</h4>
+                                    <p class="post-comment-message">I concur with that statement, this comment is just
+                                        here to acknowledge what was written before.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="post-new-comment">
+                            <img class="post-new-comment-img" src="/img/profile_pic_default.jpg" alt=""/>
+                            <form>
+                                <input type="text">
+                                <button type="submit"></button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>  <!-- einde post -->
 
             </div>  <!-- einde student feef section -->
 
